57c64d62924e9f1ee633052a11640a38f5726c0e
ricardo <ricardo@ingeniabit.com>
2023-08-20 13:20:43 -0600

    Agregando los primeros archivos del proyecto

---
+++ src/pages/PermissionsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PermissionsScreen = () => {
+    return (
+        <View>
+            <Text>Permissions Screen</Text>
+        </View>
+    )
+}
+
+export default PermissionsScreen
